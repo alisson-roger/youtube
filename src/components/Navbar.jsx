9e94159c17904a6c96252cb61fdce575047dc2e2
Alisson Roger Barbosa Lima <50409048+alisson-roger@users.noreply.github.com>
2022-08-11 12:06:21 -0300

    navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { Search, ShoppingCartOutlined } from '@material-ui/icons';
+import { Badge } from '@material-ui/core';
+
+const Container = styled.div`
+  height: 6rem;
+`;
+
+const Wrapper = styled.div`
+  padding: 1rem 2rem;
+  display: flex;
+  align-items: center
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+
+const Language = styled.span`
+  font-size: 1rem;
+  cursor: pointer;
+`;
+
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 1rem;
+  padding: 0.3rem;
+`;
+
+const Input = styled.input`
+  border: none;
+`;
+const Center = styled.div`
+  flex: 1;
+  text-align: center;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const MenuItem = styled.div`
+  font-size: 1rem;
+  cursor: pointer;
+  margin-left: 2rem;
+`;
+
+const Navbar = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input />
+            <Search />
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo>LAMA.</Logo>
+        </Center>
+        <Right>
+          <MenuItem> Register</MenuItem>
+          <MenuItem> Sign In</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlined />
+            </Badge>
+          </MenuItem>
+        </Right>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Navbar;
